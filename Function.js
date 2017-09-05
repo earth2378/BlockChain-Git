45e835c33651c10f7ed23569da11e944f8673440
earth2378 <earth2378@hotmail.com>
2017-09-05 10:12:21 +0700

    Add function addPeer

--- Function.js
+++ Function.js
@@ -44,3 +44,9 @@ function checkGasLimit(){
     console.log("Block : "+count+"\t, GasLimit : "+gasLimit);
   }
 }
+
+//Shotcut for add node
+function addPeer(input){
+  var ip = input;
+  admin.addPeer("enode://e8f4778e06bb937b13d49c7fd696aaa80ed780ec5c587bf2cd4442d9c08a60772b4fb3d0ad732818c179b1b9d77da2d062733046ebf1b2cd2bdc082820f53cb6@"+ip+":30303?discport=0");
+}
