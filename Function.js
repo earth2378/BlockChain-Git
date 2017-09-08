7c0d70d0e6b2c6a5cb0b9cc2f4811e255406e7a3
earth2378 <earth2378@hotmail.com>
2017-09-08 14:48:25 +0700

    c

--- Function.js
+++ Function.js
@@ -64,3 +64,7 @@ function addPeer(input){
   var ip = input;
   admin.addPeer("enode://e8f4778e06bb937b13d49c7fd696aaa80ed780ec5c587bf2cd4442d9c08a60772b4fb3d0ad732818c179b1b9d77da2d062733046ebf1b2cd2bdc082820f53cb6@"+ip+":30303?discport=0");
 }
+
+function unlockAcc(i) {
+  personal.unlockAccount(eth.accounts[i]);
+}
